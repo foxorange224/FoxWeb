0d15bb442f461077c9a0947f3ede5dfac51ff8e8
FoxOrange224 <foxorange224@gmail.com>
2025-12-28 18:45:35 -0400

    Update FoxWeb

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((names) => {
-      for (let name of names) caches.delete(name);
-    }).then(() => self.clients.claim())
-  );
-});
-// No agregues nada más, especialmente ningún bloque "fetch"
